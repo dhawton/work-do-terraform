58b30eb07c925af6258889d12234d5d9a919e9d3
Daniel Hawton <daniel@hawton.org>
2021-08-05 15:10:47 -0500

    Few more tweaks

--- scripts/rancher.sh
+++ scripts/rancher.sh
@@ -7,7 +7,7 @@ function set_rancher_admin_password() {
     admin_password=$2
     if [[ -z "$admin_password" ]]; then
         echo "Warning: admin password not provided"
-        admin_password=$(openssl rand -base64 16)
+        admin_password=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
         echo "--- Using admin password: $admin_password"
     fi
 
@@ -17,15 +17,15 @@ function set_rancher_admin_password() {
         exit 1
     fi
 
-    curl -ks "https://$rancher_api/v3/users?action=changePassword" -H "Content-Type: application/json" -d '{"oldPassword":"'"$defaultPass"'", "newPassword":"'"${admin_password}"'"}' -H "Authorization: Bearer $login_token"
-    curl -ks "https://$rancher_api/v3/settings/server-url" -H "Content-Type: application/json" -d '{"url":"https://'"${rancher_api}"'"}' -H "Authorization: Bearer $login_token"
+    curl -ks "https://$rancher_api/v3/users?action=changepassword" -H "Content-Type: application/json" -d '{"currentPassword":"'"$defaultPass"'", "newPassword":"'"${admin_password}"'"}' -H "Authorization: Bearer $login_token"
+    curl -ks "https://$rancher_api/v3/settings/server-url" -X PUT -H "Content-Type: application/json" -d '{"name":"server-url","value":"https://'"${rancher_api}"'"}' -H "Authorization: Bearer $login_token"
 }
 
 function get_rancher_token() {
     rancher_api=$1
     admin_password=$2
     local __resultvar=$2
-    login_token=$(curl -ks "https://$rancher_api/v3-public/localProviders/local?action=login" -H "Content-Type: application/json" -d '{"username":"admin", "password":"'"$admin_password"'"}' | jq -r '.token')
+    login_token=$(curl -ks "https://$rancher_api/v3-public/localProviders/local?action=login" -X POST -H "Content-Type: application/json" -d '{"username":"admin", "password":"'"$admin_password"'"}' | jq -r '.token')
     if [[ -z "$login_token" ]]; then
         echo "Error: Failed to login to Rancher"
         exit 1
